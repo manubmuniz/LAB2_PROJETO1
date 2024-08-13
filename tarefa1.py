cd9ec7a002775d67b3fc366060420955ff4d2039
Manuela Belem Muniz <manubmuniz@gmail.com>
2024-08-13 09:34:11 -0300

    Segundo Passo

--- tarefa1.py
+++ tarefa1.py
@@ -1,1 +1,6 @@
-print("Hello World")
+def cumprimento(texto):
+    return f"Olá, {texto}"
+
+# Chamar a função com seu nome completo
+resultado = cumprimento("Manuela Belem Muniz")
+print(resultado)
